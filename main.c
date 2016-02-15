4baef43615ceaad2e67c527a1a55b0be244cea90
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-14 22:36:02 -0500

    Added the -b option, comments, and an inputFile variable

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <argp.h>
 
-int argCount = 1;
+int argCount = 1; // Argument Count
+char *inputFile; // The Input File
 
 static int parseOption(int key, char *arg, struct argp_state *state) {
-	static int count = 0;
+	static int count = 0; // Argument Count
 	switch (key) {
-		case 'd':
+		case 'b':
+			// Do Something
+			break;
+		case 'o':
 			// Do Something
 			break;
 		case ARGP_KEY_ARG:
-			count++;
-			// Do Something
+			count++; // Increment the argument count
+			if (count == 1) // It is the first and only argument
+				inputFile = arg; // Set the inputFile string
 			break;
 		case ARGP_KEY_END: 
-			if (count < argCount)
+			if (count < argCount) // Not Enough Arguments
 				argp_failure(state, 1, 0, "too few arguments");
-			else if (count > argCount)
+			else if (count > argCount) // Too Many Arguments
 				argp_failure(state, 1, 0, "too many arguments"); 
 			break;
 	}
@@ -28,14 +33,17 @@ typedef enum {
 	packed,
 	shared,
 	std140
-} LayoutType;
+} LayoutType; // Different Layout Types
 
 struct argp_option options[] = {
+	{"block", 'b', "BLOCK", 0, "GLSL Uniform Block"},
 	{"output", 'o', "FILE", 0, "Output File"},
 	{0}
-};
-struct argp argp = {options, parseOption, "INPUT FILE"}; 
+}; // Options
+struct argp argp = {options, parseOption, "INPUT"}; // Argp 
 
 int main(int argc, char **argv) {
-	return argp_parse(&argp, argc, argv, 0, 0, &argCount);
+	argp_parse(&argp, argc, argv, 0, 0, &argCount); // Parse Arguments and Options
+	printf("%s\n", inputFile);
+	return 0;
 }
