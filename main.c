572b361acba0619ed47c8c35ab97e34eb3f0f41f
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2016-02-15 15:35:14 -0500

    Made a list of types that could be present in a uniform block minus samplers

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <argp.h>
 
 int parseInputFile(char *file);
@@ -39,7 +40,7 @@ typedef enum {
 } LayoutType; // Different Layout Types
 
 struct argp_option options[] = {
-	{"block", 'b', "BLOCK", 0, "GLSL Uniform Block"},
+	{"block", 'b', "BLOCK", 0, "GLSL Uniform Block (Chooses First by Default)"},
 	{"output", 'o', "FILE", 0, "Output File"},
 	{0}
 }; // Options
@@ -51,14 +52,28 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+char *types[] = {
+	"bool", "int", "uint", "float", "double", // Scalars
+	"bvec2", "bvec3", "bvec4", // Boolean Vectors
+	"ivec2", "ivec3", "ivec4", // Int Vectors
+	"uvec2", "uvec3", "uvec4", // Uint Vectors
+	"vec2", "vec3", "vec4", // Float Vectors
+	"dvec2", "dvec3", "dvec4", // Double Vectors
+	"mat2", "mat3", "mat4", // NxN Matrices
+	"mat2x2", "mat2x3", "mat2x4", // 2xN Matrices
+	"mat3x2", "mat3x3", "mat3x4", // 3xN Matrices
+	"mat4x2", "mat4x3", "mat4x4", // 4xN Matrices
+};
+
 int parseInputFile(char *file) {
 	printf("Parsing file: %s\n", file); 
 	FILE *input;
-	const char *delimiters = " ()\n\t";
+	const char *delimiters = " ()\n\t"; // Delimiters for parsing file
 	char temp[512]; // Temporary String
 	int line = 1; // Line Number
 	int blockLine; // Line of Block
-	int endLine; // Line of End of Block 
+	int endLine; // Line of End of Block
+	bool inBlock = false;
 	if ((input = fopen(file, "r")) == NULL) { // Open the File
 		printf("Failed to open file\n");
 		return -1; // Fail
@@ -73,9 +88,11 @@ int parseInputFile(char *file) {
 			tempString = strtok(NULL, delimiters); // Retrieve Next Token
 		} 
 		if (strcmp(tokenized[0], "layout") == 0 && strcmp(tokenized[1], "std140") == 0 && strcmp(tokenized[2], "uniform") == 0) { // Find UBO
+			inBlock = true;
 			blockLine = line; // Set the line of the block 
 		}
 		if (strcmp(tokenized[0], "};") == 0) {
+			inBlock = false;
 			endLine = line;
 			line++;
 			break;
